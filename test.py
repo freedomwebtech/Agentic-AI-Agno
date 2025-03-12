a2e66e33065b420e17168b0982d9508c3f7b3746
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-03-12 14:35:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from agno.agent import Agent
 from agno.models.google import Gemini
 
 agent = Agent(
-    model=Gemini(id="gemini-2.0-flash-exp", search=True,api_key=""),
+    model=Gemini(id="gemini-2.0-flash", search=True,api_key=""),
     show_tool_calls=True,
     markdown=True,
 )
