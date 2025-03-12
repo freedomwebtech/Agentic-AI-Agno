88a352091f08fdaf234985e8282903d15f8ba960
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-03-12 12:16:40 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,17 @@
 
+from pathlib import Path
+
+from agno.agent import Agent
+from agno.media import Video
+from agno.models.google import Gemini
+
+agent = Agent(
+    model=Gemini(id=""),
+    markdown=True,
+)
+
+# Please download "GreatRedSpot.mp4" using
+# wget https://storage.googleapis.com/generativeai-downloads/images/GreatRedSpot.mp4
+video_path = Path(__file__).parent.joinpath("cost.mp4")
+
+agent.print_response("Tell me about this video", videos=[Video(filepath=video_path)])
